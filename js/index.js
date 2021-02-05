a33c749021e3d57dd6dc4e8aa7b5876e24f2d0f7
Ravi soni <ets.raviindb@gmail.com>
2021-02-05 19:27:44 +0530

    Some Changes in localstorage functions

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 let comments = [];
 let CurrentPage = 1;
 let mod = 0;
-// debugger;
+
     function addComment() {
       let commentTxt = document.getElementById("commentTxt").value;
-      let comment = { message: commentTxt, 
-                    datetime: new Date().toLocaleString()};
+      let comment = { 
+                    message: commentTxt, 
+                    datetime: new Date().toLocaleString()
+                    };
       comments.push(comment);
       renderComments();
       paginate();
-      // updateStorage();
+      LocalsetItem();
     }
 
     function renderComments(){
@@ -21,42 +23,28 @@ let mod = 0;
           html += `<li class="list-group-item input-list">${obj.message} &nbsp;&nbsp; ${obj.datetime}</li>`;
         })
       document.getElementById("list").innerHTML = html;
+      LocalgetItem();
     }
 
     function handleJumpToPage(jumpTo){
-      CurrentPage = jumpTo;
-      renderComments();
-      paginate();
+       CurrentPage = jumpTo;
+       renderComments();
+       paginate();
     }
 
-    // function nextPage() {
-    // currentPage += 1;
-    // paginate();
-    // }
-// function previousPage() {
-//     currentPage -= 1;
-//     loadList();
-// }
+    function handleNext(){
+        if(CurrentPage <= mod-1)
+            CurrentPage = CurrentPage+1;
+            renderComments();
+            paginate();
+    }
 
-    // function handlePrev(){
-    //   if(CurrentPage ===1)
-    //     document.getElementById("prev").classList.add(".disabledBtn");
-    //     // element.classList.add("disabledBtn");
-    //     return
-    //      // document.getElementById("prev").classList.remove("disabledBtn");
-    //       CurrentPage = CurrentPage-1
-    //       renderComments();
-    //       // paginate();
-
-    // }
-    // function handleNext(){
-    //   if(CurrentPage === mod-1)
-    //     return;
-    //   CurrentPage = CurrentPage+1;
-    //   renderComments();
-    //   // paginate();
-
-    // }
+    function handlePrev(){
+        if(CurrentPage > 1)
+            CurrentPage = CurrentPage-1;
+            renderComments();
+            paginate();
+     }
 
     function paginate() {
       let count = comments.length;
@@ -74,7 +62,7 @@ let mod = 0;
       else{
         mod = Math.round(count/5);
       }
-      for (var i = 0; i < mod; i++)
+      for (let i = 0; i < mod; i++)
       {
         str += `<li class="page-item" onclick="handleJumpToPage(${i+1})">
                     <a class="page-link ${CurrentPage==i+1 ? "activePage" : ""}">${i+1}</a>
@@ -84,5 +72,55 @@ let mod = 0;
       paginator.innerHTML = str;
     }
 
+    function LocalsetItem(){
+        // let display = document.getElementById("commentTxt").value;
+        localStorage.setItem('comments', JSON.stringify(comments));
+    }
 
+    function LocalgetItem(){
+        let display = localStorage.getItem('comments');
+        // document.getElementById("displayItem").value = display;
+        document.getElementById('displayItem').innerHTML = display;
+
+    }
+
+
+
+
+
+
+
+    // function LocalgetItem(){
+    //     JSON.parse(localStorage.getItem( 'comments'));
+    // }
+
+    // function LocalsetItem(){
+    //     localStorage.setItem( 'comments', JSON.stringify(comments));
+    // }
 // })();   
+    // function handlePrev(){
+    //   if(CurrentPage > 1)
+    //     // document.getElementById("prev").classList.add(".disabledBtn");
+    //     // element.classList.add("disabledBtn");
+    //     // return
+    //      // document.getElementById("prev").classList.remove("disabledBtn");
+    //       CurrentPage = CurrentPage-1
+    //       renderComments();
+    //       paginate();
+    // }
+    // function handleNext(){
+    //   if(CurrentPage === mod-1)
+    //     return;
+    //   CurrentPage = CurrentPage+1;
+    //   renderComments();
+    //   // paginate();
+
+    // }
+// function mySave() {
+//         var myContent = document.getElementById("myTextarea").value;
+//         localStorage.setItem("myContent", myContent);
+//       }
+//       function myLoad() {
+//         var myContent = localStorage.getItem("myContent");
+//         document.getElementById("myTextarea").value = myContent;
+//       }
